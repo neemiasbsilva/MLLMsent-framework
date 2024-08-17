3b7d7655334f1ca9d776e20d91489d989d891a96
neemiasbsilva <neemiasbsilva@gmail.com>
2024-08-17 07:19:27 -0300

    [New] add bart-mnli experiments for p2- and alpha 4

--- run.sh
+++ run.sh
@@ -6,6 +6,7 @@
 # python scripts/train.py --config experiments/bart-experiment-p3-alpha4/config.yaml # for bart-mnli
 # python scripts/train.py --config experiments/bart-experiment-p2plus-alpha4/config.yaml # for bart-mnli
 python scripts/train.py --config experiments/bart-experiment-p2neg-alpha4/config.yaml # for bart-mnli
+# python scripts/train.py --config experiments/bart-experiment-p5-alpha5/config.yaml # for bart-mnli
 # python scripts/train.py --config experiments/experiment1/config.yaml # for distil-bert
 # python scripts/train.py --config experiments/experiment2/config.yaml 
 # python scripts/train.py --config experiments/experiment3/config.yaml
--- scripts/train.py
+++ scripts/train.py
@@ -198,8 +198,8 @@ def train(config):
     ### Load experiments for alpha 4
     # df = load_dataframe("data/percept_dataset_alpha4_p5.csv")
     # df = load_dataframe("data/percept_dataset_alpha4_p3.csv")
-    df = load_dataframe("data/percept_dataset_alpha4_p2plus.csv")
-    # df = load_dataframe("data/percept_dataset_alpha4_p2neg.csv")
+    # df = load_dataframe("data/percept_dataset_alpha4_p2plus.csv")
+    df = load_dataframe("data/percept_dataset_alpha4_p2neg.csv")
 
     ### Load experiments for alpha 5
     # df = load_dataframe("data/percept_dataset_alpha5_p5.csv")
