4ef25b294d5803bfbb2c56152d98fb823823944e
neemiasbsilva <neemiasbsilva@gmail.com>
2024-10-04 10:19:43 -0300

    [New] add openai gpt4 + distil-bert alpha4 p2neg

--- scripts/train.py
+++ scripts/train.py
@@ -222,8 +222,9 @@ def train(config):
     # df = load_dataframe("data/percept_dataset_alpha4_p3.csv")
     # df = load_dataframe("data/gpt4-openai-classify/percept_dataset_alpha4_p3.csv")
     # df = load_dataframe("data/percept_dataset_alpha4_p2plus.csv")
-    df = load_dataframe("data/gpt4-openai-classify/percept_dataset_alpha4_p2plus.csv")
+    # df = load_dataframe("data/gpt4-openai-classify/percept_dataset_alpha4_p2plus.csv")
     # df = load_dataframe("data/percept_dataset_alpha4_p2neg.csv")
+    df = load_dataframe("data/gpt4-openai-classify/percept_dataset_alpha4_p2neg.csv")
 
     ### Load experiments for alpha 5
     # df = load_dataframe("data/percept_dataset_alpha5_p5.csv")
