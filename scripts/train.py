a08d4d4e4b8b1cc8eef7f0a0d90ae58aee30dc3b
neemiasbsilva <neemiasbsilva@gmail.com>
2024-10-04 11:09:47 -0300

    [New] add openai gpt4 + distil-bert alpha5 p3

--- scripts/train.py
+++ scripts/train.py
@@ -228,8 +228,9 @@ def train(config):
 
     ### Load experiments for alpha 5
     # df = load_dataframe("data/percept_dataset_alpha5_p5.csv")
-    df = load_dataframe("data/gpt4-openai-classify/percept_dataset_alpha5_p5.csv")
+    # df = load_dataframe("data/gpt4-openai-classify/percept_dataset_alpha5_p5.csv")
     # df = load_dataframe("data/percept_dataset_alpha5_p3.csv")
+    df = load_dataframe("data/gpt4-openai-classify/percept_dataset_alpha5_p3.csv")
     # df = load_dataframe("data/percept_dataset_alpha5_p2plus.csv")
     # df = load_dataframe("data/percept_dataset_alpha5_p2neg.csv")
 
