8c7df2189bd4568c717e39851f5425ba941e63f9
neemiasbsilva <neemiasbsilva@gmail.com>
2024-10-11 17:00:47 -0300

    [Update] reformatted scripts using black package

--- models/model.py
+++ models/model.py
@@ -3,11 +3,12 @@ from transformers import DistilBertModel
 from transformers import BertModel
 from transformers import AutoModelForSequenceClassification
 
+
 class DistilBERTModel(torch.nn.Module):
     def __init__(self, bert_path, class_size):
         super().__init__()
         self.distilbert = DistilBertModel.from_pretrained(bert_path)
-        
+
         self.classifier = torch.nn.Sequential(
             torch.nn.Linear(768, 1024),
             # torch.nn.BatchNorm1d(1024),
@@ -15,15 +16,14 @@ class DistilBERTModel(torch.nn.Module):
             # torch.nn.Dropout(0.5),
             # torch.nn.Linear(1024, 768),
             torch.nn.ReLU(),
-            torch.nn.Linear(1024, class_size)
+            torch.nn.Linear(1024, class_size),
         )
-    
 
     def forward(self, ids, mask, token_type_id):
         output = self.distilbert(ids, attention_mask=mask)
         # shape: (batch_size, seq_length, bert_hidden_dim)
-        last_hidden_state = output.last_hidden_state 
-        
+        last_hidden_state = output.last_hidden_state
+
         # indexing the tensor containing the hidden representations
         CLS_token_state = last_hidden_state[:, 0, :]
 
@@ -31,13 +31,13 @@ class DistilBERTModel(torch.nn.Module):
         out = self.classifier(CLS_token_state)
 
         return out
-    
+
 
 class BERTFinetuningModel(torch.nn.Module):
     def __init__(self, bert_path):
         super().__init__()
         self.bert = BertModel.from_pretrained(bert_path)
-        
+
         self.classifier = torch.nn.Sequential(
             torch.nn.Linear(1024, 1024),
             torch.nn.BatchNorm1d(1024),
@@ -45,10 +45,8 @@ class BERTFinetuningModel(torch.nn.Module):
             torch.nn.Dropout(0.5),
             torch.nn.Linear(1024, 768),
             torch.nn.ReLU(),
-            torch.nn.Linear(768, 3)
+            torch.nn.Linear(768, 3),
         )
-        
-    
 
     def forward(self, ids, mask, token_type_id):
         output = self.bert(ids, mask, token_type_id)
@@ -57,11 +55,12 @@ class BERTFinetuningModel(torch.nn.Module):
 
         # indexing the tensor containing the hidden representations
         CLS_token_state = last_hidden_state[:, 0, :]
-        
+
         # passing this representation through our custom classifier
         out = self.classifier(CLS_token_state)
 
         return out
+
 
 class RoBERTaFinetuningModel(torch.nn.Module):
     def __init__(self, model_path):
@@ -71,7 +70,7 @@ class RoBERTaFinetuningModel(torch.nn.Module):
         # self.dropout = torch.nn.Dropout(0.1)
         # self.relu = torch.nn.ReLU()
         # self.out = torch.nn.Linear(768, 3)
-    
+
     def forward(self, ids, mask, token_type_id):
         return self.roberta(ids, mask).logits
         # print(output)
@@ -80,5 +79,3 @@ class RoBERTaFinetuningModel(torch.nn.Module):
         # x = self.pre_classifier(pooled_output)
         # x = self.dropout(self.relu(x))
         # return out
-
-
--- scripts/train.py
+++ scripts/train.py
@@ -1,14 +1,19 @@
 import argparse
 import os
 import sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 import pandas as pd
 import numpy as np
 import torch
 from tqdm import tqdm
 from utils.other_utils import load_config
 from utils.data_loader import data_loader
-from transformers import DistilBertTokenizer, BertTokenizer, AutoModelForSequenceClassification
+from transformers import (
+    DistilBertTokenizer,
+    BertTokenizer,
+    AutoModelForSequenceClassification,
+)
 from transformers import AutoTokenizer, AutoConfig
 from models.model import DistilBERTModel, BERTFinetuningModel, RoBERTaFinetuningModel
 from transformers import BartForSequenceClassification, BartTokenizerFast
@@ -21,9 +26,10 @@ from transformers import AdamW
 from evaluate import evaluate
 import torch.nn.functional as F
 
-# from test import 
+# from test import
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
+
 
 def load_dataframe(path):
     return pd.read_csv(path)
@@ -31,6 +37,7 @@ def load_dataframe(path):
 
 # def loss_fn(class_weights, outputs, targets):
 #     return torch.nn.CrossEntropyLoss(weight=class_weights,reduction='mean')(outputs, targets)
+
 
 def val(log_dir, model, dataloader, loss_fn, kfold, df_metrics, model_name):
     ## Validation phase
@@ -55,13 +62,21 @@ def val(log_dir, model, dataloader, loss_fn, kfold, df_metrics, model_name):
 
     accuracy = accuracy_score(targets, preds)
     f1 = f1_score(targets, preds, average="weighted")
-    loss = total_loss/len(dataloader)
-    
-    df_metrics = pd.concat([df_metrics, pd.DataFrame({"kfold": [kfold+1], "accuracy": [accuracy], "f1_score": [f1]
-                                                         })], axis=0)
+    loss = total_loss / len(dataloader)
+
+    df_metrics = pd.concat(
+        [
+            df_metrics,
+            pd.DataFrame(
+                {"kfold": [kfold + 1], "accuracy": [accuracy], "f1_score": [f1]}
+            ),
+        ],
+        axis=0,
+    )
     df_metrics.to_csv(os.path.join(log_dir, f"test_logs.csv"), index=False)
 
     return df_metrics
+
 
 class ContrastiveLoss(torch.nn.Module):
     def __init__(self, margin=1.0):
@@ -71,21 +86,34 @@ class ContrastiveLoss(torch.nn.Module):
     def forward(self, output1, output2, label):
         # Distância euclidiana entre as duas representações
         euclidean_distance = F.pairwise_distance(output1, output2)
-        
+
         # Perda contrativa
         loss = torch.mean(
-            (1 - label) * torch.pow(euclidean_distance, 2) +
-            label * torch.pow(torch.clamp(self.margin - euclidean_distance, min=0.0), 2)
+            (1 - label) * torch.pow(euclidean_distance, 2)
+            + label
+            * torch.pow(torch.clamp(self.margin - euclidean_distance, min=0.0), 2)
         )
         return loss
 
 
-def fit(model, class_weights, epochs, optimizer, train_dl, val_dl, log_dir, checkpoint_dir, name_arch, fold, model_name):
-    loss_fn = torch.nn.CrossEntropyLoss(weight=class_weights, reduction='mean')
+def fit(
+    model,
+    class_weights,
+    epochs,
+    optimizer,
+    train_dl,
+    val_dl,
+    log_dir,
+    checkpoint_dir,
+    name_arch,
+    fold,
+    model_name,
+):
+    loss_fn = torch.nn.CrossEntropyLoss(weight=class_weights, reduction="mean")
     # for param in model.distilbert.parameters():
     # for param in model.distilbert.parameters():
     # for param in model.distilbert.parameters():
-        # param.requires_grad = False
+    # param.requires_grad = False
     model.to(device)
     torch.manual_seed(42)
     np.random.seed(42)
@@ -114,7 +142,7 @@ def fit(model, class_weights, epochs, optimizer, train_dl, val_dl, log_dir, chec
             # exit()
             loss = loss_fn(outputs, targets)
             loss.backward()
-            
+
             # Adjust learning weights
             optimizer.step()
             # scheduler.step()
@@ -142,15 +170,14 @@ def fit(model, class_weights, epochs, optimizer, train_dl, val_dl, log_dir, chec
                 preds_val.extend(torch.argmax(outputs, axis=1).tolist())
                 targets_val.extend(targets.tolist())
 
-        
         ## Calculate metrics
         accuracy_train = accuracy_score(targets_train, preds_train)
         f1_train = f1_score(targets_train, preds_train, average="weighted")
-        loss_train = total_loss_train/len(train_dl)
+        loss_train = total_loss_train / len(train_dl)
 
         accuracy_val = accuracy_score(targets_val, preds_val)
         f1_val = f1_score(targets_val, preds_val, average="weighted")
-        loss_val = total_loss_val/len(val_dl)
+        loss_val = total_loss_val / len(val_dl)
 
         # Write logs to file
         # print(f"Epoch {epoch+1}/{epochs}:")
@@ -163,24 +190,39 @@ def fit(model, class_weights, epochs, optimizer, train_dl, val_dl, log_dir, chec
             f"Validation Loss: {loss_val:.4f}, Accuracy: {accuracy_val:.4f}, "
             f"F1-score: {f1_val:.4f}\n"
         )
-        with open(log_file, 'a') as f:
+        with open(log_file, "a") as f:
             f.write(log_entry)
 
-        
-        if ( stop_train == 10): 
+        if stop_train == 10:
             print("Validation accuracy don't change for 10 epochs, finish training")
             break
-        df_metrics = pd.concat([df_metrics, pd.DataFrame({"epoch": [epoch+1], "train_accuracy": [accuracy_train], "train_f1_score": [f1_train],
-                                                         "val_accuracy": [accuracy_val], "val_f1_score": [f1_val]
-                                                         })], axis=0)
-        df_metrics.to_csv(os.path.join(log_dir, f"training_logs_{fold+1:02d}.csv"), index=False)
+        df_metrics = pd.concat(
+            [
+                df_metrics,
+                pd.DataFrame(
+                    {
+                        "epoch": [epoch + 1],
+                        "train_accuracy": [accuracy_train],
+                        "train_f1_score": [f1_train],
+                        "val_accuracy": [accuracy_val],
+                        "val_f1_score": [f1_val],
+                    }
+                ),
+            ],
+            axis=0,
+        )
+        df_metrics.to_csv(
+            os.path.join(log_dir, f"training_logs_{fold+1:02d}.csv"), index=False
+        )
 
         # scheduler.step()
         # Save checkpoint
-        if (best_acc < accuracy_val):
+        if best_acc < accuracy_val:
             best_acc = accuracy_val
             stop_train = 0
-            checkpoint_path = os.path.join(checkpoint_dir, f"best_checkpoint_{name_arch.split('/')[0]}.pt")
+            checkpoint_path = os.path.join(
+                checkpoint_dir, f"best_checkpoint_{name_arch.split('/')[0]}.pt"
+            )
             torch.save(model.state_dict(), checkpoint_path)
         stop_train += 1
 
@@ -197,13 +239,12 @@ def train(config):
     max_len = config["max_len"]
     model_path = config["model_path"]
     log_dir = config["log_dir"]
-    checkpoint_dir = config['checkpoint_dir']
-    model_name = config['model_name']
+    checkpoint_dir = config["checkpoint_dir"]
+    model_name = config["model_name"]
 
     step_size = 1
     gamma = 0.1
-    name_arch = model_path.split('-')[0]
-
+    name_arch = model_path.split("-")[0]
 
     # load_datasets and preprocessing pytorch dataloader
     ### Load experiments for alpha 3
@@ -229,20 +270,20 @@ def train(config):
     ### Load experiments for alpha 5
     # df = load_dataframe("data/percept_dataset_alpha5_p5.csv")
     # df = load_dataframe("data/gpt4-openai-classify/percept_dataset_alpha5_p5.csv")
-    # df = load_dataframe("data/percept_dataset_alpha5_p3.csv")
+    df = load_dataframe("data/percept_dataset_alpha5_p3.csv")
     # df = load_dataframe("data/gpt4-openai-classify/percept_dataset_alpha5_p3.csv")
     # df = load_dataframe("data/percept_dataset_alpha5_p2plus.csv")
     # df = load_dataframe("data/gpt4-openai-classify/percept_dataset_alpha5_p2plus.csv")
-    df = load_dataframe("data/percept_dataset_alpha5_p2neg.csv")
+    # df = load_dataframe("data/percept_dataset_alpha5_p2neg.csv")
     # df = load_dataframe("data/gpt4-openai-classify/percept_dataset_alpha5_p2neg.csv")
 
     train_val_df, test_df = train_test_split(df, test_size=0.2, random_state=42)
     train_val_df = df.copy()
 
+    test_df = pd.DataFrame(
+        {"text": test_df.text.to_list(), "sentiment": test_df.sentiment.to_list()}
+    )
 
-
-    test_df = pd.DataFrame({"text": test_df.text.to_list(), "sentiment": test_df.sentiment.to_list()})
-    
     # train_df = load_dataframe("data/train/aug_train.csv")
 
     train_params = {
@@ -250,34 +291,36 @@ def train(config):
         "shuffle": True,
     }
     # val_df = load_dataframe("data/validation/val.csv")
-    
-    val_params = {
-        "batch_size": batch_size,
-        "shuffle": True
-    }
 
-    if (model_name == "distil-bert"):
+    val_params = {"batch_size": batch_size, "shuffle": True}
+
+    if model_name == "distil-bert":
         # # Define the tokenizer
-        tokenizer = DistilBertTokenizer.from_pretrained(
-            model_path,
-            do_lower_case=True
-        )
+        tokenizer = DistilBertTokenizer.from_pretrained(model_path, do_lower_case=True)
         # tokenizer = BertTokenizer.from_pretrained(
         #     model_path,
         #     # do_lower_case=True
         # )
         # tokenizer = AutoTokenizer.from_pretrained(model_path)
         # config = AutoConfig.from_pretrained(model_path)
-        
+
         kfold = KFold(n_splits=5, shuffle=True, random_state=42)
         df_metrics = pd.DataFrame([])
         for fold, (train_idx, val_idx) in enumerate(kfold.split(train_val_df)):
             print(f"Fold {fold + 1}")
-            train_df = pd.DataFrame({"text": train_val_df["text"].iloc[train_idx].to_list(), 
-                                     "sentiment": train_val_df["sentiment"].iloc[train_idx].to_list()})
-            val_df = pd.DataFrame({"text": train_val_df["text"].iloc[val_idx].to_list(), 
-                                     "sentiment": train_val_df["sentiment"].iloc[val_idx].to_list()})
-            
+            train_df = pd.DataFrame(
+                {
+                    "text": train_val_df["text"].iloc[train_idx].to_list(),
+                    "sentiment": train_val_df["sentiment"].iloc[train_idx].to_list(),
+                }
+            )
+            val_df = pd.DataFrame(
+                {
+                    "text": train_val_df["text"].iloc[val_idx].to_list(),
+                    "sentiment": train_val_df["sentiment"].iloc[val_idx].to_list(),
+                }
+            )
+
             train_dl = data_loader(train_df, tokenizer, max_len, train_params)
             val_dl = data_loader(val_df, tokenizer, max_len, val_params)
             test_dl = data_loader(test_df, tokenizer, max_len, val_params)
@@ -289,25 +332,61 @@ def train(config):
             print(model)
 
             # optimizer = torch.optim.Adam(params=model.parameters(), lr=learning_rate, weight_decay=0.01)
-            optimizer = torch.optim.AdamW(params=model.parameters(), lr=learning_rate, weight_decay=1e-6)
+            optimizer = torch.optim.AdamW(
+                params=model.parameters(), lr=learning_rate, weight_decay=1e-6
+            )
             # optimizer = torch.optim.SGD(params=model.parameters(), lr=learning_rate, momentum=.9)
             # scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=learning_rate, steps_per_epoch=len(train_dl), epochs=epochs)
-            
+
             class_size = train_df.sentiment.value_counts().sort_index().to_list()
-            if (len(class_size) == 3):
-                class_weights = torch.Tensor([1/class_size[0], 1/class_size[1], 1/class_size[2]]).type(torch.float).to(device)
-            elif (len(class_size) == 2):
-                class_weights = torch.Tensor([1/class_size[0], 1/class_size[1]]).type(torch.float).to(device)
+            if len(class_size) == 3:
+                class_weights = (
+                    torch.Tensor(
+                        [1 / class_size[0], 1 / class_size[1], 1 / class_size[2]]
+                    )
+                    .type(torch.float)
+                    .to(device)
+                )
+            elif len(class_size) == 2:
+                class_weights = (
+                    torch.Tensor([1 / class_size[0], 1 / class_size[1]])
+                    .type(torch.float)
+                    .to(device)
+                )
             else:
-                class_weights = torch.Tensor([1/class_size[0], 1/class_size[1], 1/class_size[2], 1/class_size[3], 1/class_size[4]]).type(torch.float).to(device)
+                class_weights = (
+                    torch.Tensor(
+                        [
+                            1 / class_size[0],
+                            1 / class_size[1],
+                            1 / class_size[2],
+                            1 / class_size[3],
+                            1 / class_size[4],
+                        ]
+                    )
+                    .type(torch.float)
+                    .to(device)
+                )
 
-                                        
-
-            model, loss_fn = fit(model, class_weights, epochs, optimizer, train_dl, val_dl, log_dir, checkpoint_dir, name_arch, fold, model_name)
+            model, loss_fn = fit(
+                model,
+                class_weights,
+                epochs,
+                optimizer,
+                train_dl,
+                val_dl,
+                log_dir,
+                checkpoint_dir,
+                name_arch,
+                fold,
+                model_name,
+            )
 
             # df_metrics = val(log_dir, model, test_dl, loss_fn, fold, df_metrics, model_name)
-            df_metrics = val(log_dir, model, val_dl, loss_fn, fold, df_metrics, model_name)
-        
+            df_metrics = val(
+                log_dir, model, val_dl, loss_fn, fold, df_metrics, model_name
+            )
+
         print(f'Mean F1-score: {np.mean(df_metrics["f1_score"].to_numpy())*100:.2f}%')
     else:
         # tokenizer = AutoTokenizer.from_pretrained(model_path)
@@ -318,35 +397,85 @@ def train(config):
         df_metrics = pd.DataFrame([])
         for fold, (train_idx, val_idx) in enumerate(kfold.split(train_val_df)):
             print(f"Fold {fold + 1}")
-            train_df = pd.DataFrame({"text": train_val_df["text"].iloc[train_idx].to_list(), 
-                                     "sentiment": train_val_df["sentiment"].iloc[train_idx].to_list()})
-            val_df = pd.DataFrame({"text": train_val_df["text"].iloc[val_idx].to_list(), 
-                                     "sentiment": train_val_df["sentiment"].iloc[val_idx].to_list()})
-            
+            train_df = pd.DataFrame(
+                {
+                    "text": train_val_df["text"].iloc[train_idx].to_list(),
+                    "sentiment": train_val_df["sentiment"].iloc[train_idx].to_list(),
+                }
+            )
+            val_df = pd.DataFrame(
+                {
+                    "text": train_val_df["text"].iloc[val_idx].to_list(),
+                    "sentiment": train_val_df["sentiment"].iloc[val_idx].to_list(),
+                }
+            )
+
             train_dl = data_loader(train_df, tokenizer, max_len, train_params)
             val_dl = data_loader(val_df, tokenizer, max_len, val_params)
             test_dl = data_loader(test_df, tokenizer, max_len, val_params)
 
             class_size = train_df.sentiment.value_counts().sort_index().to_list()
 
-
-            if (len(class_size) == 3):
-                class_weights = torch.Tensor([1/class_size[0], 1/class_size[1], 1/class_size[2]]).type(torch.float).to(device)
-            elif (len(class_size) == 2):
-                class_weights = torch.Tensor([1/class_size[0], 1/class_size[1]]).type(torch.float).to(device)
+            if len(class_size) == 3:
+                class_weights = (
+                    torch.Tensor(
+                        [1 / class_size[0], 1 / class_size[1], 1 / class_size[2]]
+                    )
+                    .type(torch.float)
+                    .to(device)
+                )
+            elif len(class_size) == 2:
+                class_weights = (
+                    torch.Tensor([1 / class_size[0], 1 / class_size[1]])
+                    .type(torch.float)
+                    .to(device)
+                )
             else:
-                class_weights = torch.Tensor([1/class_size[0], 1/class_size[1], 1/class_size[2], 1/class_size[3], 1/class_size[4]]).type(torch.float).to(device)
-        
-            model = BartForSequenceClassification.from_pretrained('facebook/bart-large-mnli', num_labels=len(class_size), ignore_mismatched_sizes=True)
+                class_weights = (
+                    torch.Tensor(
+                        [
+                            1 / class_size[0],
+                            1 / class_size[1],
+                            1 / class_size[2],
+                            1 / class_size[3],
+                            1 / class_size[4],
+                        ]
+                    )
+                    .type(torch.float)
+                    .to(device)
+                )
+
+            model = BartForSequenceClassification.from_pretrained(
+                "facebook/bart-large-mnli",
+                num_labels=len(class_size),
+                ignore_mismatched_sizes=True,
+            )
             model.to(device)
-            print(model)  
+            print(model)
 
-            optimizer = torch.optim.AdamW(params=model.parameters(), lr=learning_rate, weight_decay=1e-6)            
+            optimizer = torch.optim.AdamW(
+                params=model.parameters(), lr=learning_rate, weight_decay=1e-6
+            )
 
-            model, loss_fn = fit(model, class_weights, epochs, optimizer, train_dl, val_dl, log_dir, checkpoint_dir, name_arch, fold, model_name)
+            model, loss_fn = fit(
+                model,
+                class_weights,
+                epochs,
+                optimizer,
+                train_dl,
+                val_dl,
+                log_dir,
+                checkpoint_dir,
+                name_arch,
+                fold,
+                model_name,
+            )
 
-            df_metrics = val(log_dir, model, test_dl, loss_fn, fold, df_metrics, model_name)
-        
+            # df_metrics = val(log_dir, model, test_dl, loss_fn, fold, df_metrics, model_name)
+            df_metrics = val(
+                log_dir, model, val_dl, loss_fn, fold, df_metrics, model_name
+            )
+
         print(f'Mean F1-score: {np.mean(df_metrics["f1_score"].to_numpy())*100:.2f}%')
 
         # data = train_df.text
@@ -362,7 +491,9 @@ def train(config):
 if __name__ == "__main__":
     # Parse command line arguments
     parser = argparse.ArgumentParser(description="Train the DL model.")
-    parser.add_argument("--config", type=str, default="experiments/experiment1/config.yaml")
+    parser.add_argument(
+        "--config", type=str, default="experiments/experiment1/config.yaml"
+    )
     args = parser.parse_args()
 
     # Load configuration
