a339df52d6389930b8702cdfdd5c3dc6553be8ad
neemiasbsilva <neemiasbsilva@gmail.com>
2025-02-07 14:41:32 -0300

    [Update] shell run

--- run.sh
+++ run.sh
@@ -1,41 +1,47 @@
-# python scripts/train.py --config experiments/distil-experiment-p5-alpha3/config.yaml # for distil-bert
-# python scripts/train.py --config experiments/distil-experiment-p3-alpha3/config.yaml # for distil-bert
-# python scripts/train.py --config experiments/distil-experiment-p2plus-alpha3/config.yaml # for distil-bert
-# python scripts/train.py --config experiments/distil-experiment-p2neg-alpha3/config.yaml # for distil-bert
-# python scripts/train.py --config experiments/distil-experiment-p5-alpha4/config.yaml # for distil-bert
-# python scripts/train.py --config experiments/distil-experiment-p3-alpha4/config.yaml # for distil-bert
-# python scripts/train.py --config experiments/distil-experiment-p2plus-alpha4/config.yaml # for distil-bert
-# python scripts/train.py --config experiments/distil-experiment-p2neg-alpha4/config.yaml # for distil-bert
-# python scripts/train.py --config experiments/distil-experiment-p5-alpha5/config.yaml # for distil-bert
-# python scripts/train.py --config experiments/distil-experiment-p3-alpha5/config.yaml # for distil-bert
-# python scripts/train.py --config experiments/distil-experiment-p2plus-alpha5/config.yaml # for distil-bert
-python scripts/train.py --config experiments/distil-experiment-p2neg-alpha5/config.yaml # for distil-bert
-# python scripts/train.py --config experiments/bart-experiment-p5-alpha3/config.yaml # for bart-mnli
-# python scripts/train.py --config experiments/bart-experiment-p3-alpha3/config.yaml # for bart-mnli
-# python scripts/train.py --config experiments/bart-experiment-p2plus-alpha3/config.yaml # for bart-mnli
-# python scripts/train.py --config experiments/bart-experiment-p2neg-alpha3/config.yaml # for bart-mnli
-# python scripts/train.py --config experiments/bart-experiment-p5-alpha4/config.yaml # for bart-mnli
-# python scripts/train.py --config experiments/bart-experiment-p3-alpha4/config.yaml # for bart-mnli
-# python scripts/train.py --config experiments/bart-experiment-p2plus-alpha4/config.yaml # for bart-mnli
-# python scripts/train.py --config experiments/bart-experiment-p2neg-alpha4/config.yaml # for bart-mnli
-# python scripts/train.py --config experiments/bart-experiment-p5-alpha5/config.yaml # for bart-mnli
-# python scripts/train.py --config experiments/bart-experiment-p3-alpha5/config.yaml # for bart-mnli
-# python scripts/train.py --config experiments/bart-experiment-p2plus-alpha5/config.yaml # for bart-mnli
-# python scripts/train.py --config experiments/bart-experiment-p2neg-alpha5/config.yaml # for bart-mnli
-# python scripts/train.py --config experiments/openai-distil-experiment-p5-alpha3/config.yaml # for distil-bert openai
-# python scripts/train.py --config experiments/openai-distil-experiment-p3-alpha3/config.yaml # for distil-bert openai
-# python scripts/train.py --config experiments/openai-distil-experiment-p2plus-alpha3/config.yaml # for distil-bert openai
-# python scripts/train.py --config experiments/openai-distil-experiment-p2neg-alpha3/config.yaml # for distil-bert openai
-# python scripts/train.py --config experiments/openai-distil-experiment-p5-alpha4/config.yaml # for distil-bert openai
-# python scripts/train.py --config experiments/openai-distil-experiment-p3-alpha4/config.yaml # for distil-bert openai
-# python scripts/train.py --config experiments/openai-distil-experiment-p2plus-alpha4/config.yaml # for distil-bert openai
-# python scripts/train.py --config experiments/openai-distil-experiment-p2neg-alpha4/config.yaml # for distil-bert openai
-# python scripts/train.py --config experiments/openai-distil-experiment-p5-alpha5/config.yaml # for distil-bert openai
-# python scripts/train.py --config experiments/openai-distil-experiment-p3-alpha5/config.yaml # for distil-bert openai
-# python scripts/train.py --config experiments/openai-distil-experiment-p2plus-alpha5/config.yaml # for distil-bert openai
-# python scripts/train.py --config experiments/openai-distil-experiment-p2neg-alpha5/config.yaml # for distil-bert openai
-# python scripts/train.py --config experiments/openai-bart-experiment-p5-alpha3/config.yaml # for bart-mnli openai
-# python scripts/train.py --config experiments/experiment1/config.yaml # for distil-bert
-# python scripts/train.py --config experiments/experiment2/config.yaml 
-# python scripts/train.py --config experiments/experiment3/config.yaml
-# python scripts/train.py --config experiments/experiment4/config.yaml
+# Sigma 5
+python scripts/train.py --config /home/neemias/PerceptSent-LLM-approach/experiments/modernbert-experiment-p5-alpha5/config.yaml
+python scripts/train.py --config /home/neemias/PerceptSent-LLM-approach/experiments/modernbert-experiment-p3-alpha5/config.yaml
+python scripts/train.py --config /home/neemias/PerceptSent-LLM-approach/experiments/modernbert-experiment-p2plus-alpha5/config.yaml
+python scripts/train.py --config /home/neemias/PerceptSent-LLM-approach/experiments/modernbert-experiment-p2neg-alpha5/config.yaml
+# Sigma 4
+python scripts/train.py --config /home/neemias/PerceptSent-LLM-approach/experiments/modernbert-experiment-p5-alpha4/config.yaml
+python scripts/train.py --config /home/neemias/PerceptSent-LLM-approach/experiments/modernbert-experiment-p3-alpha4/config.yaml
+python scripts/train.py --config /home/neemias/PerceptSent-LLM-approach/experiments/modernbert-experiment-p2plus-alpha4/config.yaml
+python scripts/train.py --config /home/neemias/PerceptSent-LLM-approach/experiments/modernbert-experiment-p2neg-alpha4/config.yaml
+# Sigma 3
+python scripts/train.py --config /home/neemias/PerceptSent-LLM-approach/experiments/modernbert-experiment-p5-alpha3/config.yaml
+python scripts/train.py --config /home/neemias/PerceptSent-LLM-approach/experiments/modernbert-experiment-p3-alpha3/config.yaml
+python scripts/train.py --config /home/neemias/PerceptSent-LLM-approach/experiments/modernbert-experiment-p2plus-alpha3/config.yaml
+python scripts/train.py --config /home/neemias/PerceptSent-LLM-approach/experiments/modernbert-experiment-p2neg-alpha3/config.yaml
+
+# Sigma 5
+python scripts/train.py --config /home/neemias/PerceptSent-LLM-approach/experiments/openai-modernbert-experiment-p5-alpha5/config.yaml
+python scripts/train.py --config /home/neemias/PerceptSent-LLM-approach/experiments/openai-modernbert-experiment-p3-alpha5/config.yaml
+python scripts/train.py --config /home/neemias/PerceptSent-LLM-approach/experiments/openai-modernbert-experiment-p2plus-alpha5/config.yaml
+python scripts/train.py --config /home/neemias/PerceptSent-LLM-approach/experiments/openai-modernbert-experiment-p2neg-alpha5/config.yaml
+# Sigma 4
+python scripts/train.py --config /home/neemias/PerceptSent-LLM-approach/experiments/openai-modernbert-experiment-p5-alpha4/config.yaml
+python scripts/train.py --config /home/neemias/PerceptSent-LLM-approach/experiments/openai-modernbert-experiment-p3-alpha4/config.yaml
+python scripts/train.py --config /home/neemias/PerceptSent-LLM-approach/experiments/openai-modernbert-experiment-p2plus-alpha4/config.yaml
+python scripts/train.py --config /home/neemias/PerceptSent-LLM-approach/experiments/openai-modernbert-experiment-p2neg-alpha4/config.yaml
+# Sigma 3
+python scripts/train.py --config /home/neemias/PerceptSent-LLM-approach/experiments/openai-modernbert-experiment-p5-alpha3/config.yaml
+python scripts/train.py --config /home/neemias/PerceptSent-LLM-approach/experiments/openai-modernbert-experiment-p3-alpha3/config.yaml
+python scripts/train.py --config /home/neemias/PerceptSent-LLM-approach/experiments/openai-modernbert-experiment-p2plus-alpha3/config.yaml
+python scripts/train.py --config /home/neemias/PerceptSent-LLM-approach/experiments/openai-modernbert-experiment-p2neg-alpha3/config.yaml
+
+# Sigma 5
+python scripts/train.py --config /home/neemias/PerceptSent-LLM-approach/experiments/deepseek-modernbert-experiment-p5-alpha5/config.yaml
+python scripts/train.py --config /home/neemias/PerceptSent-LLM-approach/experiments/deepseek-modernbert-experiment-p3-alpha5/config.yaml
+python scripts/train.py --config /home/neemias/PerceptSent-LLM-approach/experiments/deepseek-modernbert-experiment-p2plus-alpha5/config.yaml
+python scripts/train.py --config /home/neemias/PerceptSent-LLM-approach/experiments/deepseek-modernbert-experiment-p2neg-alpha5/config.yaml
+# Sigma 4
+python scripts/train.py --config /home/neemias/PerceptSent-LLM-approach/experiments/deepseek-modernbert-experiment-p5-alpha4/config.yaml
+python scripts/train.py --config /home/neemias/PerceptSent-LLM-approach/experiments/deepseek-modernbert-experiment-p3-alpha4/config.yaml
+python scripts/train.py --config /home/neemias/PerceptSent-LLM-approach/experiments/deepseek-modernbert-experiment-p2plus-alpha4/config.yaml
+python scripts/train.py --config /home/neemias/PerceptSent-LLM-approach/experiments/deepseek-modernbert-experiment-p2neg-alpha4/config.yaml
+# Sigma 3
+python scripts/train.py --config /home/neemias/PerceptSent-LLM-approach/experiments/deepseek-modernbert-experiment-p5-alpha3/config.yaml
+python scripts/train.py --config /home/neemias/PerceptSent-LLM-approach/experiments/deepseek-modernbert-experiment-p3-alpha3/config.yaml
+python scripts/train.py --config /home/neemias/PerceptSent-LLM-approach/experiments/deepseek-modernbert-experiment-p2plus-alpha3/config.yaml
+python scripts/train.py --config /home/neemias/PerceptSent-LLM-approach/experiments/deepseek-modernbert-experiment-p2neg-alpha3/config.yaml
