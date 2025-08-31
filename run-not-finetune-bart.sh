90d0b75ff750be91f677bfcdeb4849fb5a08088f
neemiasbsilva <neemiasbsilva@gmail.com>
2025-08-31 21:46:36 +0000

    feat: update sh scripts for run the approach

--- run-not-finetune-bart.sh
+++ run-not-finetune-bart.sh
@@ -1,48 +1,14 @@
-# Pre-trained - BART - OpenSource
-## Sigma 5
-# python scripts/train_gpu1.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-not-finetuning/bart-experiment-p2neg-alpha5/config.yaml
-# python scripts/train_gpu1.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-not-finetuning/bart-experiment-p2plus-alpha5/config.yaml
-# python scripts/train_gpu1.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-not-finetuning/bart-experiment-p3-alpha5/config.yaml # Done
-# python scripts/train_gpu1.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-not-finetuning/bart-experiment-p5-alpha5/config.yaml # Done
-# ## Sigma 4
-# python scripts/train_gpu1.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-not-finetuning/bart-experiment-p2neg-alpha4/config.yaml
-# python scripts/train_gpu1.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-not-finetuning/bart-experiment-p2plus-alpha4/config.yaml
-# python scripts/train_gpu1.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-not-finetuning/bart-experiment-p3-alpha4/config.yaml
-# python scripts/train_gpu1.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-not-finetuning/bart-experiment-p5-alpha4/config.yaml
-# ## Sigma 3
-# python scripts/train_gpu1.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-not-finetuning/bart-experiment-p2neg-alpha3/config.yaml
-# python scripts/train_gpu1.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-not-finetuning/bart-experiment-p2plus-alpha3/config.yaml
-# python scripts/train_gpu1.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-not-finetuning/bart-experiment-p3-alpha3/config.yaml # Done
-python scripts/train_gpu1.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-not-finetuning/bart-experiment-p5-alpha3/config.yaml
-# Pre-trained - BART - Openai
-## Sigma 5
-# python scripts/train_gpu1.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-not-finetuning/openai-bart-experiment-p2neg-alpha5/config.yaml
-# python scripts/train_gpu1.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-not-finetuning/openai-bart-experiment-p2plus-alpha5/config.yaml
-python scripts/train_gpu1.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-not-finetuning/openai-bart-experiment-p3-alpha5/config.yaml
-python scripts/train_gpu1.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-not-finetuning/openai-bart-experiment-p5-alpha5/config.yaml
-## Sigma 4
-# python scripts/train_gpu1.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-not-finetuning/openai-bart-experiment-p2neg-alpha4/config.yaml
-# python scripts/train_gpu1.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-not-finetuning/openai-bart-experiment-p2plus-alpha4/config.yaml
-# python scripts/train_gpu1.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-not-finetuning/openai-bart-experiment-p3-alpha4/config.yaml
-# python scripts/train_gpu1.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-not-finetuning/openai-bart-experiment-p5-alpha4/config.yaml
-## Sigma 3
-# python scripts/train_gpu1.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-not-finetuning/openai-bart-experiment-p2neg-alpha3/config.yaml
-# python scripts/train_gpu1.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-not-finetuning/openai-bart-experiment-p2plus-alpha3/config.yaml
-python scripts/train_gpu1.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-not-finetuning/openai-bart-experiment-p3-alpha3/config.yaml
-python scripts/train_gpu1.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-not-finetuning/openai-bart-experiment-p5-alpha3/config.yaml
-# Pre-trained - BART - Deepseek
-## Sigma 5
-# python scripts/train_gpu1.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-not-finetuning/deepseek-bart-experiment-p2neg-alpha5/config.yaml
-# python scripts/train_gpu1.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-not-finetuning/deepseek-bart-experiment-p2plus-alpha5/config.yaml
-python scripts/train_gpu1.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-not-finetuning/deepseek-bart-experiment-p3-alpha5/config.yaml
-python scripts/train_gpu1.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-not-finetuning/deepseek-bart-experiment-p5-alpha5/config.yaml
-## Sigma 4
-# python scripts/train_gpu1.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-not-finetuning/deepseek-bart-experiment-p2neg-alpha4/config.yaml
-# python scripts/train_gpu1.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-not-finetuning/deepseek-bart-experiment-p2plus-alpha4/config.yaml
-# python scripts/train_gpu1.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-not-finetuning/deepseek-bart-experiment-p3-alpha4/config.yaml
-# python scripts/train_gpu1.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-not-finetuning/deepseek-bart-experiment-p5-alpha4/config.yaml
-## Sigma 3
-# python scripts/train_gpu1.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-not-finetuning/deepseek-bart-experiment-p2neg-alpha3/config.yaml
-# python scripts/train_gpu1.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-not-finetuning/deepseek-bart-experiment-p2plus-alpha3/config.yaml
-python scripts/train_gpu1.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-not-finetuning/deepseek-bart-experiment-p3-alpha3/config.yaml
-python scripts/train_gpu1.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-not-finetuning/deepseek-bart-experiment-p5-alpha3/config.yaml
+
+python scripts/train_gpu0.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-not-finetuning/openai-bart-experiment-p5-alpha5/config.yaml
+python scripts/train_gpu0.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-not-finetuning/deepseek-bart-experiment-p5-alpha5/config.yaml
+python scripts/train_gpu0.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-not-finetuning/bart-experiment-p5-alpha5/config.yaml 
+python scripts/train_gpu0.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-not-finetuning/openai-bart-experiment-p5-alpha3/config.yaml
+python scripts/train_gpu0.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-not-finetuning/deepseek-bart-experiment-p5-alpha3/config.yaml
+python scripts/train_gpu0.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-not-finetuning/bart-experiment-p5-alpha3/config.yaml
+
+python scripts/train_gpu0.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-not-finetuning/openai-bart-experiment-p3-alpha5/config.yaml
+python scripts/train_gpu0.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-not-finetuning/deepseek-bart-experiment-p3-alpha5/config.yaml
+python scripts/train_gpu0.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-not-finetuning/bart-experiment-p3-alpha5/config.yaml 
+python scripts/train_gpu0.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-not-finetuning/openai-bart-experiment-p3-alpha3/config.yaml
+python scripts/train_gpu0.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-not-finetuning/deepseek-bart-experiment-p3-alpha3/config.yaml
+python scripts/train_gpu0.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-not-finetuning/bart-experiment-p3-alpha3/config.yaml 
--- run-twitter.sh
+++ run-twitter.sh
@@ -3,10 +3,10 @@
 # python scripts/train.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-twitter/openai-distil-experiment-p2neg-alpha5/config.yaml
 # python scripts/train.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-twitter/openai-distil-experiment-p2neg-alpha3/config.yaml
 # python scripts/train.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-twitter/openai-distil-experiment-p2plus-alpha3/config.yaml
-python scripts/train.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-twitter/openai-modernbert-experiment-p2plus-alpha5/config.yaml
-python scripts/train.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-twitter/openai-modernbert-experiment-p2neg-alpha5/config.yaml
-python scripts/train.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-twitter/openai-modernbert-experiment-p2neg-alpha3/config.yaml
-python scripts/train.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-twitter/openai-modernbert-experiment-p2plus-alpha3/config.yaml
+python scripts/train_gpu1.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-twitter/openai-modernbert-experiment-p2plus-alpha5/config.yaml
+python scripts/train_gpu1.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-twitter/openai-modernbert-experiment-p2neg-alpha5/config.yaml
+python scripts/train_gpu1.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-twitter/openai-modernbert-experiment-p2neg-alpha3/config.yaml
+python scripts/train_gpu1.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-twitter/openai-modernbert-experiment-p2plus-alpha3/config.yaml
 # python scripts/train.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-twitter/openai-bart-experiment-p2neg-alpha5/config.yaml
 # python scripts/train.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-twitter/openai-bart-experiment-p2plus-alpha5/config.yaml
 # python scripts/train.py --config /mnt/raid5/neemias/PerceptSent-LLM-approach/experiments-twitter/openai-bart-experiment-p2plus-alpha3/config.yaml
